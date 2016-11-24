1e0dded6b997e559b314d643bb3ae7450cdb00b8
483446 <483446@728fff39-6be0-d74b-8c93-aa940da9b35a>
2016-11-24 15:09:09 +0000

    Stable dissertation

---
+++ 483446/Development Project/Capture the Campus Template++/Game/UDPServer/UDPServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    public class UDPServer
+    {
+        public static void Main()
+        {
+            const int listenPort = 11000;
+
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    Console.WriteLine("Received broadcast from " + groupEP.ToString() + ": " + Encoding.ASCII.GetString(bytes, 0, bytes.Length) + "\r\n");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+}
