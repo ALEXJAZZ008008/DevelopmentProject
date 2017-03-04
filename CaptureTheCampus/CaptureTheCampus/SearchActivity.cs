200397fd1319056bece0a0404d9744bd258e7be1
Alexander Whitehead <alexandercwhitehead@gmail.com>
2017-03-04 00:24:14 +0000

    Limited paths to play area

---
+++ CaptureTheCampus/CaptureTheCampus/SearchActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+
+namespace CaptureTheCampus
+{
+    [Activity(Label = "@string/SearchActivityLabel", Icon = "@drawable/icon")]
+    public class SearchActivity : Activity
+    {
+        private string searchType;
+
+        private string text;
+
+        private Host host;
+        private Join join;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Log.Debug("OnCreate", "OnCreate called, initializing views...");
+
+            // Set our view from the "menu" layout resource
+            SetContentView(Resource.Layout.Search);
+
+            searchType = Intent.GetStringExtra("searchType");
+
+            if(searchType == "Host")
+            {
+                host = new Host();
+
+                text = "Host";
+            }
+            else
+            {
+                if (searchType == "Join")
+                {
+                    join = new Join();
+
+                    text = "Join";
+                }
+            }
+        }
+
+        private void GoToSetActivity(string gameType)
+        {
+            Log.Debug("GoToSetActivity", "GoToSetActivity called, going to SetActivity...");
+
+            Intent intent = new Intent(this, typeof(SetActivity));
+            intent.PutExtra("gameType", gameType);
+            StartActivity(intent);
+
+            Finish();
+        }
+    }
+}
